80d558b7f4cc1d22f46e477ab2abf9fc469c24e5
Julia <juliagawlak18@gmail.com>
2024-11-15 18:17:14 -0300

    Refatorando main.

--- src/Exemplo.java
+++ src/Exemplo.java
@@ -3,7 +3,12 @@ public class Exemplo {
     private static void sayHello(){
         System.out.println("Olá mundo");
     }
+
+    private static void say(){
+        System.out.println("Julia Linda");
+    }
     public static void main(String[] args) {
         sayHello();
+        say();
     }
 }
