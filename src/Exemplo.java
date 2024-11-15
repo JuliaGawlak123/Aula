743bdb4e9aec3da8d8b8d9b3d3bffd5cd63a8742
Julia <juliagawlak18@gmail.com>
2024-11-15 18:28:12 -0300

    removido o arquivo txt

--- src/Exemplo.java
+++ src/Exemplo.java
@@ -1,5 +1,12 @@
 
 public class Exemplo {
+    private static void sayHelloManyTimes(int times){
+        for(int i = 0; i< times;i++){
+            say();
+            sayHello();
+        }
+    }
+
     private static void sayHello(){
         System.out.println("Olá mundo");
     }
@@ -8,7 +15,6 @@ public class Exemplo {
         System.out.println("Julia Linda");
     }
     public static void main(String[] args) {
-        sayHello();
-        say();
+        sayHelloManyTimes(5);
     }
 }
